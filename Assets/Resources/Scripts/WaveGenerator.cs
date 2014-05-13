9f8ef1866934e7c47d1b4b212322b55e32966ff8
Matt Tytel <mtytel@leapmotion.com>
2014-05-12 20:11:47 -0700

    Squashed 'v2/BlockDestruction2/' content from commit 32e8ac4
    git-subtree-dir: v2/BlockDestruction2
    git-subtree-split: 32e8ac46ec78be0e0fd1de4685cdaacbb9085788

---
+++ Assets/Resources/Scripts/WaveGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnWave", 0, 3);
+	}
+
+	void SpawnWave() {
+		GameObject obj = Instantiate(Resources.Load("Prefabs/BlockGame/Wave")) as GameObject;
+		obj.transform.position = new Vector3(0, 0, 30);
+		Rigidbody [] children = obj.GetComponentsInChildren<Rigidbody>();
+
+		for(int i = 0; i < children.Length; ++i) {
+			children[i].velocity = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-3.0f, -8.0f));
+			children[i].angularVelocity = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		}
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+BlockDestruction2
+=====
+<img src="https://leapmotion-leapdev-production.s3.amazonaws.com/uploads/library/thumbnail_image/44374ce9-e61e-4cac-a027-bcc841e1c3cf.jpg">
+
+##Description
+Push, flick, wave around the green blocks to hit the white blocks, close your fist to bring back the green blocks to its initial stage.
+
+##API Methods
+* [Finger Joints](https://developer.leapmotion.com/documentation/skeletal/csharp/api/Leap.Finger.html#id50)
+* [Hands](https://developer.leapmotion.com/documentation/skeletal/csharp/api/Leap.Hand.html)
