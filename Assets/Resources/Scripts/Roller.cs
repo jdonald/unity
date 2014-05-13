6cdf7f24456a2eaf25cb68c2d35ef40e311030c3
Matt Tytel <mtytel@leapmotion.com>
2014-05-12 20:12:44 -0700

    Squashed 'v2/DancingRobits/' content from commit ff75040
    git-subtree-dir: v2/DancingRobits
    git-subtree-split: ff750406a55b577f43c6d461e6c82e1ad8ad0fc3

---
+++ Assets/Resources/Scripts/Roller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Roller : MonoBehaviour {
+
+  public Transform upright;
+
+	void Start () {
+    rigidbody.maxAngularVelocity = 100.0f;
+    Physics.IgnoreCollision(collider, upright.collider, true);
+	}
+	
+	void Update () {
+    float rotation_angle = 0;
+    Vector3 rotation_axis;
+    Quaternion force_quaternion = Quaternion.FromToRotation(Vector3.up,
+                                                            upright.transform.up);
+    force_quaternion.ToAngleAxis(out rotation_angle, out rotation_axis);
+
+    // You get knocked down, don't get up again.
+    if (rotation_angle > 60)
+      return;
+
+    // Vector3 upright_angular_vel = upright.rigidbody.angularVelocity;
+    Vector3 velocity_diff = rigidbody.velocity - upright.rigidbody.velocity;
+    Vector3 velocity_diff_rotation = Vector3.Cross(velocity_diff, Vector3.up);
+    Vector3 velocity_rotation = Vector3.Cross(rigidbody.velocity, Vector3.up);
+
+    // This is pretty much hacked together. Change these but beware.
+    Vector3 force = 80.0f * Mathf.Tan(rotation_angle / 180.0f * Mathf.PI) * rotation_axis +
+                    80.0f * velocity_diff_rotation -
+                    6.0f * velocity_rotation;
+    rigidbody.AddTorque(force);
+	}
+}
